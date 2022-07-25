4017b201ccf6ea2bb5538970c2275256ab8007ec
Urvashi2304 <109856786+Urvashi2304@users.noreply.github.com>
2022-07-25 06:27:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, this is a beautiful World!'));
-app.get('/', (req, res) => res.send('Hello, I am so beautiful... i am sooooo happyyyyyy!'));
+app.get('/', (req, res) => res.send('Hello, this is a beautiful World, i am so pretty, i love myself, i am so cute, hahahahahahahahahah, bla bla bla, this is a web app, oh yes :)!'));
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
